3608dcd477c27a8cda5d99f97d23903a56558025
Rudra pratap nahak <121797461+RudraPratapNahak@users.noreply.github.com>
2024-01-19 06:43:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict" // Treate all javascript code as newer version
+
+// alert("Hello")  // We are using nodejs, not browser
+
+console.log(3+ 3)  //code readability should be high
+
+console.log("Rudra")
+
+let name = "Rudra"
+let age = 21
+let loggedin = True
+
+// type of datatype
+
+// number - 2 to power 53
+// bigint - for larger numbers
+// string - always use ""
+// boolean - true / flase
+
+//  null - it is a standalone value 
+// null output gives as a object
+
+// undefined -  value not assigned
+// undefined - when we declare variable but don't assign any value
+
+// symbol - unique
+
+// object
+
+console.log(typeof undefined);  //it is undefined
+console.log(typeof null);     //it is object
